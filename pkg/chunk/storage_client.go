975b006c3c138e0e42bfa64b1ae62d52d7f5ea85
Jacob Lisi <jacob.t.lisi@gmail.com>
2018-09-20 17:13:35 -0400

    add stream chunks to storage interface
    Signed-off-by: Jacob Lisi <jacob.t.lisi@gmail.com>

--- pkg/chunk/storage_client.go
+++ pkg/chunk/storage_client.go
@@ -16,6 +16,9 @@ type StorageClient interface {
 	// For storing and retrieving chunks.
 	PutChunks(ctx context.Context, chunks []Chunk) error
 	GetChunks(ctx context.Context, chunks []Chunk) ([]Chunk, error)
+
+	StreamChunks(ctx context.Context, params StreamBatch, chunkChan chan []Chunk) error
+	NewStreamBatch() StreamBatch
 }
 
 // WriteBatch represents a batch of writes.
@@ -33,4 +36,9 @@ type ReadBatchIterator interface {
 	Next() bool
 	RangeValue() []byte
 	Value() []byte
+}
+
+// StreamBatch represents the configuration for streaming chunks
+type StreamBatch interface {
+	Add(tableName, userID string, from, to int)
 }
