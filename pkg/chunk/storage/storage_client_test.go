b65dd75b273f830c24d5e5beef0d21c6a9e3bef6
Jacob Lisi <jacob.t.lisi@gmail.com>
2018-10-22 15:47:52 -0400

    update streamer interface to be embedded in streamer struct, update tests, fix bugs
    Signed-off-by: Jacob Lisi <jacob.t.lisi@gmail.com>

--- pkg/chunk/storage/storage_client_test.go
+++ pkg/chunk/storage/storage_client_test.go
@@ -60,54 +60,19 @@ func TestChunksBasic(t *testing.T) {
 	})
 }
 
-func TestStreamChunks(t *testing.T) {
+// TestStreamer ensures Streamer clients honors the userID batch option
+func TestStreamer(t *testing.T) {
 	forAllFixtures(t, func(t *testing.T, client chunk.StorageClient, schema chunk.SchemaConfig) {
 		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 		defer cancel()
-		_, chunks, err := testutils.CreateChunks(0, 2000)
-		require.NoError(t, err)
 
-		err = client.PutChunks(ctx, chunks)
-		require.NoError(t, err)
-
-		batch := client.NewStreamBatch()
+		batch := client.NewStreamer()
 		if batch == nil {
 			return
 		}
-		tablename := schema.ChunkTables.TableFor(model.Now().Add(-time.Hour))
-		batch.Add(tablename, "userID", 0, 240)
+		tablename := schema.ChunkTableFor(model.Now().Add(-time.Hour))
+		batch.Add(tablename, "userIDNew", 0, 240)
 
-		var retrievedChunks []chunk.Chunk
-		var wg sync.WaitGroup
-		out := make(chan []chunk.Chunk)
-		go func() {
-			wg.Add(1)
-			for c := range out {
-				retrievedChunks = append(retrievedChunks, c...)
-			}
-			wg.Done()
-		}()
-
-		err = client.StreamChunks(context.Background(), batch, out)
-		require.NoError(t, err)
-
-		close(out)
-		wg.Wait()
-		require.Equal(t, 2000, len(retrievedChunks))
-
-		sort.Sort(ByKey(retrievedChunks))
-		sort.Sort(ByKey(chunks))
-		for j := 0; j < len(retrievedChunks); j++ {
-			require.Equal(t, chunks[j].ExternalKey(), retrievedChunks[j].ExternalKey(), strconv.Itoa(j))
-		}
-	})
-}
-
-// TestStreamChunksByUserID ensures StreamChunks clients honors the userID batch option
-func TestStreamChunksByUserID(t *testing.T) {
-	forAllFixtures(t, func(t *testing.T, client chunk.StorageClient, schema chunk.SchemaConfig) {
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancel()
 		_, chunks, err := testutils.CreateChunks(0, 2000)
 		require.NoError(t, err)
 
@@ -120,12 +85,9 @@ func TestStreamChunksByUserID(t *testing.T) {
 		err = client.PutChunks(ctx, chunks)
 		require.NoError(t, err)
 
-		batch := client.NewStreamBatch()
-		if batch == nil {
-			return
-		}
-		tablename := schema.ChunkTables.TableFor(model.Now().Add(-time.Hour))
-		batch.Add(tablename, "userIDNew", 0, 240)
+		size, err := batch.Size(context.Background())
+		require.NoError(t, err)
+		require.Equal(t, 2000, size)
 
 		var retrievedChunks []chunk.Chunk
 		var wg sync.WaitGroup
@@ -145,7 +107,7 @@ func TestStreamChunksByUserID(t *testing.T) {
 			wg.Done()
 		}()
 
-		err = client.StreamChunks(context.Background(), batch, out)
+		err = batch.Stream(context.Background(), out)
 		require.NoError(t, err)
 
 		close(out)
